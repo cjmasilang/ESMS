4ee9f2d11a001ccab8fba563d14055c66ac6c981
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2022-12-22 21:45:30 +0800

    Create database.php

---
+++ lib/database.php
@@ -0,0 +1,20 @@
+<?php
+
+class  Database{
+
+  public $pdo;
+
+  public function __construct(){
+
+    if (!isset($this->pdo)) {
+      try {
+        $link = new PDO('mysql:host='.DB_HOST.'; dbname='.DB_NAME, DB_USER, DB_PASS);
+        $link->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $link->exec("SET CHARACTER SET utf8");
+        $this->pdo  =  $link;
+      } catch (PDOException $e) {
+        die("Connection error...".$e->getMessage());
+      }
+    }
+  }
+}
