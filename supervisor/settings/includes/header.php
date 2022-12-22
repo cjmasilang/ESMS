efc08a1e23d722fdd752ebc633607c4a71524ee0
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2022-12-22 21:47:29 +0800

    Create header.php

---
+++ supervisor/settings/includes/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Supervisor </title>
+        <link href="https://cdn.jsdelivr.net/npm/simple-datatables@latest/dist/style.css" rel="stylesheet" />
+        <link href="../css/styles.css" rel="stylesheet" />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="../assets/bootstrap.min.css">
+        <link href="assets/img/logo.png" rel="icon">
+        <link href="https://use.fontawesome.com/releases/v5.0.4/css/all.css" rel="stylesheet">
+        <link href="https://cdn.datatables.net/1.11.4/css/jquery.dataTables.min.css" rel="stylesheet">
+        <!-- <link rel="stylesheet" href="../assets/dataTables.bootstrap4.min.css"> --> 
+    </head>
+    <body class="sb-nav-fixed">
+   <?php include_once('navbar.php');?>
+        <div id="layoutSidenav">
+          <?php include_once('sidebar.php');?>
+            <div id="layoutSidenav_content">
+                <main>
