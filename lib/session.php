fcdf913f260a5565c1a7fefd41fabf078cee4f8b
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2022-12-22 21:45:37 +0800

    Add files via upload

---
+++ lib/session.php
@@ -0,0 +1,52 @@
+<?php
+
+class Session{
+
+  public static function init(){
+
+    if (version_compare(phpversion(), '5.4.0', '<')) {
+      if (session_id() == '') {
+        session_start();
+      }
+    }else{
+      if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+      }
+    }
+
+  }
+
+  public static function set($key, $val){
+    $_SESSION[$key] = $val;
+  }
+
+
+  public static function get($key){
+    if (isset($_SESSION[$key])) {
+      return $_SESSION[$key];
+    }else{
+      return false;
+    }
+  }
+
+  public static function destroy(){
+    session_destroy();
+    session_unset();
+    header('Location:login.php');
+  }
+
+
+  public static function CheckSession(){
+    if (self::get('login') == FALSE) {
+      session_destroy();
+      header('Location:login.php');
+    }
+  }
+
+  public static function CheckLogin(){
+    if (self::get("login") == TRUE) {
+      header('Location:index.php');
+    }
+  }
+
+}
