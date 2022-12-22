3019b3a56464bd5c6a328f1c6189529e99632c3e
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2022-12-22 21:33:19 +0800

    Add files via upload

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,30 @@
+  <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="dashboard.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div class="sb-nav-link-icon"><i class="fa fa-user-plus"></i></div>User Management</a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="addUser.php">Add User</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="manage-users.php">Manage User</a></li>
+                    </ul>
+                </li>
+                 
+                        
+
+ <a class="nav-link" href="bwdates-report-ds.php">
+                                <div class="sb-nav-link-icon"><i class="fa fa-calendar"></i></div>
+                               Report Generation
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
