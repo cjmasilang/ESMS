8723352c7466b95c4ac124fbae387ce4281dce29
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2022-12-22 21:47:39 +0800

    Add files via upload

---
+++ supervisor/settings/includes/sidebar.php
@@ -0,0 +1,40 @@
+  <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                        <div class="sb-sidenav-menu-heading">Core</div>
+                            <a class="nav-link" href="index.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                Dashboard
+                            </a>
+
+                            
+                 
+                 <li class="nav-item dropdown">
+                    <a class="nav-link" href="evaluation.php"> 
+                    <div class="sb-nav-link-icon"><i class="fas fa-file mr-2"></i></div>
+                    Monitoring Checklist
+                    </a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="employee_evaluation.php"> 
+                    <div class="sb-nav-link-icon"><i class="fas fa-user mr-2"></i></div>
+                    Employee Evaluation
+                    </a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div class="sb-nav-link-icon"><i class="fas fa-cog mr-2"></i></div>Settings</a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="settings.php">Monitoring Checklist</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="settings_v2.php">Employee Evaluation</a></li>
+                    </ul>
+                </li>
+                        
+                        </div>
+                    </div>
+                </nav>
+            </div>
