e864106e1873531db5f92cbe242c97f2bf3b767f
Cedric Jay <119407422+cjmasilang@users.noreply.github.com>
2022-12-22 21:44:28 +0800

    Add files via upload

---
+++ includes/sidebar.php
@@ -0,0 +1,40 @@
+  <div id="layoutSidenav_nav">
+                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                    <div class="sb-sidenav-menu">
+                        <div class="nav">
+                            <div class="sb-sidenav-menu-heading"></div>
+                            <a class="nav-link" href="welcome.php">
+                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt mr-2"></i></div>
+                                Dashboard
+                            </a>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="supervisor/evaluation.php"> 
+                    <div class="sb-nav-link-icon"><i class="fas fa-file mr-2"></i></div>
+                    Monitoring Checklist
+                    </a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link" href="supervisor/employee_evaluation.php"> 
+                    <div class="sb-nav-link-icon"><i class="fas fa-user mr-2"></i></div>
+                    Employee Evaluation
+                    </a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <div class="sb-nav-link-icon"><i class="fas fa-cog mr-2"></i></div>Settings</a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <li><a class="dropdown-item" href="supervisor/settings.php">Monitoring Checklist</a></li>
+                        <li><hr class="dropdown-divider" /></li>
+                        <li><a class="dropdown-item" href="supervisor/settings_v2.php">Employee Evaluation</a></li>
+                    </ul>
+                </li>
+
+
+
+                        </div>
+                    </div>  
+                </nav>
+            </div>
